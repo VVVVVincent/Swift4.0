63e97f8457d691e51c62aa3ddafdd7ed2bef3fdd
李慧娟 <lihj6@centaline.com.cn>
2018-12-04 16:21:14 +0800

    swiftCode6-方法
    类方法、实例方法的声明和调用

---
+++ SwiftCode6/SwiftCode6/SubViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SubViewController.swift
+//  SwiftCode6
+//
+//  Created by 李慧娟 on 2018/12/4.
+//  Copyright © 2018年 李慧娟. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        SubViewController.classFunc1()
+        SubViewController.classFunc2()
+
+
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
